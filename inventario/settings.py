626ec5b613c2e15f8690dd8d93d861a32261859f
lisagomez <lisagomez967@gmail.com>
2020-11-05 05:58:06 -0600

    message

--- inventario/settings.py
+++ inventario/settings.py
@@ -158,6 +158,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+# location where you will store your static files
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'core/static')]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
